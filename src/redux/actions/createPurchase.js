38ce5dca0718431ad0ded6a596675b86ad8cc86b
devByScotty <155840062+devByScotty@users.noreply.github.com>
2024-07-04 20:33:06 +0200

    Update createPurchase.js

--- src/redux/actions/createPurchase.js
+++ src/redux/actions/createPurchase.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const createPurchase = (purchaseData) => async (dispatch) => {
   try {
-    const response = await axios.post('http://localhost:3001/api/transactions', purchaseData);  // Correct port
+    const response = await axios.post('https://real-estate-backend-4hnr.onrender.com/api/transactions', purchaseData);  // Correct port
     dispatch({
       type: 'CREATE_PURCHASE_SUCCESS',
       payload: response.data,
